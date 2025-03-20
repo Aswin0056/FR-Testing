ba32a39b3dec8b4407b048fd46f69ab16966b066
Aswin0056 <aswinayyappan056@gmail.com>
2025-03-20 15:50:17 +0530

    Updates

---
+++ precache-manifest.c81cc0f02db3c7c79248602b9411f928.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e2a3adb5ed90e2409e3312e6991c00a",
+    "url": "/FR-Testing/index.html"
+  },
+  {
+    "revision": "c7d8fd15625eb2388645",
+    "url": "/FR-Testing/static/css/main.78f8a272.chunk.css"
+  },
+  {
+    "revision": "06a9d48b9c4bdd80e837",
+    "url": "/FR-Testing/static/js/2.96e64800.chunk.js"
+  },
+  {
+    "revision": "83c6f0a82f430d893ae6c1998b3a1c17",
+    "url": "/FR-Testing/static/js/2.96e64800.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7d8fd15625eb2388645",
+    "url": "/FR-Testing/static/js/main.e4b810e1.chunk.js"
+  },
+  {
+    "revision": "e007e8546d593d91ad1f",
+    "url": "/FR-Testing/static/js/runtime-main.93307b01.js"
+  }
+]);
